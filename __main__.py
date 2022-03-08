556e95269d6856545177973794702d4f2e362eaf
Kade Wilson <wil21131@byui.edu>
2022-03-07 19:53:54 -0700

    Changed names in constants and main
    Changed names like snake to rider and food to trail. I also updated the import lines so they work now, and increased the starting rider length to 12 from 8. Just thought that would be better to start with.

--- __main__.py
+++ __main__.py
@@ -1,27 +1,27 @@
 import constants
 
-from game.casting.cast import Cast
-from game.casting.food import Food
-from game.casting.score import Score
-from game.casting.snake import Snake
-from game.scripting.script import Script
-from game.scripting.control_actors_action import ControlActorsAction
-from game.scripting.move_actors_action import MoveActorsAction
-from game.scripting.handle_collisions_action import HandleCollisionsAction
-from game.scripting.draw_actors_action import DrawActorsAction
-from game.directing.director import Director
-from game.services.keyboard_service import KeyboardService
-from game.services.video_service import VideoService
-from game.shared.color import Color
-from game.shared.point import Point
+from cicle.game.casting.cast import Cast
+from cicle.game.casting.trail import Trail
+from cicle.game.casting.score import Score
+from cicle.game.casting.rider import Rider
+from cicle.game.scripting.script import Script
+from cicle.game.scripting.control_actors_action import ControlActorsAction
+from cicle.game.scripting.move_actors_action import MoveActorsAction
+from cicle.game.scripting.handle_collisions_action import HandleCollisionsAction
+from cicle.game.scripting.draw_actors_action import DrawActorsAction
+from cicle.game.directing.director import Director
+from cicle.game.services.keyboard_service import KeyboardService
+from cicle.game.services.video_service import VideoService
+from cicle.game.shared.color import Color
+from cicle.game.shared.point import Point
 
 
 def main():
     
     # create the cast
     cast = Cast()
-    cast.add_actor("foods", Food())
-    cast.add_actor("snakes", Snake())
+    cast.add_actor("Trails", Trail())
+    cast.add_actor("Riders", Rider())
     cast.add_actor("scores", Score())
    
     # start the game
