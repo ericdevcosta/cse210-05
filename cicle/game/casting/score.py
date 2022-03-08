132c2bdb6a7ab76553305f1dcedc9ece92441260
Joemarie <jojodenoy@gmail.com>
2022-03-08 22:28:03 +0800

    Update documentation

--- cicle/game/casting/score.py
+++ cicle/game/casting/score.py
@@ -5,10 +5,10 @@ class Score(Actor):
     """
     A record of points made or lost. 
     
-    The responsibility of Score is to keep track of the points the player has earned by eating food.
+    Each player's trails grows as they move and the responsibility of Score is to keep track of the total trail of each players.
     It contains methods for adding and getting points. Client should use get_text() to get a string 
     representation of the points earned.
-
+  
     Attributes:
         _points (int): The points earned in the game.
     """
@@ -18,7 +18,8 @@ class Score(Actor):
         self.add_points(0)
 
     def add_points(self, points):
-        """Adds the given points to the score's total points.
+        """
+        Adds the given points to the score's total points.
         
         Args:
             points (int): The points to add.
